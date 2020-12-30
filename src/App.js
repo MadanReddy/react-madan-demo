e9ca0cabb2e76cd36c89d3a3f50267cf64d20a64
MadanReddy <madanreddy78@gmail.com>
2020-12-30 13:09:56 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import Dropdown from './Dropdown'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <div>   <Dropdown />
+    </div>
+    </div>
+  );
+}
